11298914470155ca7e7a9e9f728da31afc794880
Slava Efremov <>
2022-10-23 17:38:48 +0400

    upd server url

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:8080";
+export const API_URL = "https://messages-backend-123.herokuapp.com";
 
-//"https://messages-backend-123.herokuapp.com"
+//" http://localhost:8080"
